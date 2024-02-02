663b7b52c587b6635376558b5726e23e3139718c
mohamed smith <mohara.dip@outlook.es>
2024-02-02 14:17:52 -0500

    mdp: exercise1 done

--- code/module-02/m02-start/module02.ts
+++ code/module-02/m02-start/module02.ts
@@ -5,17 +5,17 @@
     TODO: Modify the code to add types to the variable declarations. 
     The resulting JavaScript should look the same as the original example when you're done. */
 
-let firstName;
-let lastName;
-let fullName;
-let age;
-let ukCitizen;
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let age: number;
+let ukCitizen: boolean;
 
 firstName = 'Rebecca';
 lastName = 'Smith';
 age = 42;
 ukCitizen = false;
-fullName = firstname + " " + lastname;
+fullName = firstName + " " + lastName;
 
 if (ukCitizen) {
     console.log("My name is " + fullName + ", I'm " + age + ", and I'm a citizen of the United Kingdom.");
